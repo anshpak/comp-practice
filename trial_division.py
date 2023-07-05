1d70809cd148b1019a40e8304d2e392d773e550c
Andrey Shpak <anshpak.by@gmail.com>
2023-07-05 17:10:36 +0300

    refactor: delete unnecessary files

--- trial_division.py
+++
@@ -1,9 +0,0 @@
-def trial_division(n):
-    if n == 1 or n == 0:
-        return False
-    d = 2
-    while d<= n**0.5:
-        if n % d == 0:
-            return False
-        d += 1
-    return True
